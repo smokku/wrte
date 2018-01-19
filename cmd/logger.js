71fec5ba1551b134dbd4f143f74f9c07f6555a50
Tomasz Sterna <tomek@xiaoka.com>
2018-01-19 19:50:50 +0100

    minor style fixes

--- cmd/logger.js
+++ cmd/logger.js
@@ -1,5 +1,8 @@
 let init = null
-let con = null // con: channel
+const channels = {}
+
+const CONSOLE = 'con:'
+const WEBDAV = 'internal:webdav'
 
 global.console.debug('[logger] starting')
 
@@ -9,27 +12,38 @@ global.onmessage = (evt) => {
   if (data.type === 'INIT' && !init) {
     init = data.payload
     global.console.log(`[logger] started ${JSON.stringify(init.argv)}`)
-  } else if (data.type === 'CHANNEL' && data.path === 'con:') {
-    con = data.channel
+
+    global.postMessage({
+      type: 'OPEN',
+      path: CONSOLE,
+    })
+
+    global.postMessage({
+      type: 'OPEN',
+      path: WEBDAV,
+    })
+  } else if (data.type === 'CHANNEL' && data.path) {
+    channels[data.path] = data.channel
+    if (data.path === WEBDAV) {
+      global.postMessage({
+        type: 'READ',
+        channel: channels[WEBDAV],
+      })
+    }
   } else if (data.type === 'ERROR') {
     global.console.warn(`[logger] ERROR: ${JSON.stringify(data.payload)}`)
   } else {
     const msg = JSON.stringify(data)
     global.console.log(`[logger] ${msg}`)
-    if (con) {
+    if (channels[CONSOLE]) {
       global.postMessage({
-        channel: con,
+        channel: channels[CONSOLE],
         type: 'DATA',
         payload: msg,
         severity: 'NORMAL',
       })
     } else {
-      console.warn('[logger] no con: channel')
+      console.warn(`[logger] no ${CONSOLE} channel`)
     }
   }
 }
-
-global.postMessage({
-  type: 'OPEN',
-  path: 'con:',
-})
--- kernel/console.js
+++ kernel/console.js
@@ -7,8 +7,7 @@ export function init () {
 
 export function handler (path, from, msg, channel) {
   // window.console.debug('[console:]', msg.type, this.volume, this.args, path, from.pid, msg)
-  if (msg.type === 'DATA') {
-    const { payload } = msg
-    root.innerText += `${typeof payload === 'string' ? payload : JSON.stringify(payload)}\n`
+  if (msg.type === 'DATA' && typeof msg.payload === 'string') {
+    root.innerText += `${msg.payload}\n`
   }
 }
