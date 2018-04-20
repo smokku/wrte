eba58202e8f0ab9d61255b26642eb7fdb83d58ac
Tomasz Sterna <tomek@xiaoka.com>
2018-04-20 10:44:14 +0200

    Show WRTE version in window title

--- kernel/index.js
+++ kernel/index.js
@@ -1,11 +1,14 @@
+// @flow
 import test from '../lib/tape'
 import main from './main'
 
-export const name = process.env.npm_package_name
-export const version = process.env.npm_package_version
-export const build = process.env.git_build_sha
+export const name: string = process.env.npm_package_name || 'WRTE'
+export const version: string = process.env.npm_package_version || '?'
+export const build: string = process.env.git_build_sha || '?'
 
-global.console.log(`${name} ${version} (${build}) booting...`)
+const VERSION = `${name} ${version} (${build})`
+global.console.log(`${VERSION} booting...`)
+document.title = VERSION
 window.onload = main
 
 if (process.env.NODE_ENV === 'ci') {
