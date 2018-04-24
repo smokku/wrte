029dd3e38c4a9e7c54811541c56ea5f7700bc3d9
Tomasz Sterna <tomek@xiaoka.com>
2018-04-24 22:31:01 +0200

    Proper env variables

--- kernel/index.js
+++ kernel/index.js
@@ -2,9 +2,9 @@
 import test from '../lib/tape'
 import main from './main'
 
-export const name: string = process.env.npm_package_name || 'WRTE'
-export const version: string = process.env.npm_package_version || '?'
-export const build: string = process.env.git_build_sha || '?'
+export const name: string = process.env.npm_package_name
+export const version: string = process.env.npm_package_version
+export const build: string = process.env.git_build_sha
 
 const VERSION = `${name} ${version} (${build})`
 global.console.log(`${VERSION} booting...`)
