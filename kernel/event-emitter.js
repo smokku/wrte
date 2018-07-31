4fa854bf17b27760a81cdd5d15737dba4f2bbaa5
Tomasz Sterna <tomek@xiaoka.com>
2018-07-31 16:04:34 +0200

    Flow EventEmitter

--- kernel/event-emitter.js
+++ kernel/event-emitter.js
@@ -1,28 +1,43 @@
-// @flow
+// @flow strict
+export type Callback = ({} | string) => void
+
 const EVENTS = Symbol('events')
 
+/**
+ * Very simplistic EventEmitter.
+ */
 export default class EventEmitter {
+  EVENTS: { [string]: Callback }
+
   constructor () {
-    this[EVENTS] = Object.create(null)
+    // $FlowFixMe: Until flow supports computed properties
+    this[EVENTS] = Object.create(null) // FIXME: replace with WeakMap()
   }
 
-  on (event: string, cb: Function) {
+  on (event: string, cb: Callback) {
+    // $FlowFixMe: Until flow supports computed properties
     if (!this[EVENTS][event]) {
+      // $FlowFixMe: Until flow supports computed properties
       this[EVENTS][event] = [cb]
     } else {
+      // $FlowFixMe: Until flow supports computed properties
       this[EVENTS][event].push(cb)
     }
     return () => this.off(event, cb)
   }
 
-  off (event: string, cb: Function) {
+  off (event: string, cb: Callback) {
+    // $FlowFixMe: Until flow supports computed properties
     if (this[EVENTS][event]) {
+      // $FlowFixMe: Until flow supports computed properties
       this[EVENTS][event] = this[EVENTS][event].filter(fn => fn !== cb)
     }
   }
 
-  emit (event: string, data: any) {
+  emit (event: string, data: {} | string) {
+    // $FlowFixMe: Until flow supports computed properties
     if (this[EVENTS][event]) {
+      // $FlowFixMe: Until flow supports computed properties
       this[EVENTS][event].forEach(fn => fn.call(this, data))
     }
   }
