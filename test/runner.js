259bbab59b89fe859c88aa804dafcb128399448e
Tomasz Sterna <tomek@xiaoka.com>
2018-09-04 02:20:00 +0200

    Fetch __coverage__ using Puppeteer

--- test/runner.js
+++ test/runner.js
@@ -3,6 +3,7 @@
 (async () => {
   const puppeteer = require('puppeteer-core')
   const liveServer = require('live-server')
+  const fs = require('fs-extra')
 
   const { HEADFUL = false, CHROME_BIN } = process.env
 
@@ -25,9 +26,28 @@
 
   const page = await browser.newPage()
 
+  /* run in-page unit tests */
   page.on('console', msg => console.log(msg.text()))
-
   await page.goto(URL)
+  await page.waitForFunction('window.__tests_done__')
+
+  /* fetch coverage */
+  // eslint-disable-next-line no-underscore-dangle
+  const coverage = await page.evaluate(() => window.__coverage__)
+  await fs.emptyDir('.nyc_output')
+  await Promise.all(
+    Object.values(coverage).map((cov) => {
+      if (
+        cov &&
+        typeof cov === 'object' &&
+        typeof cov.path === 'string' &&
+        typeof cov.hash === 'string'
+      ) {
+        return fs.writeJson(`.nyc_output/${cov.hash}.json`, { [cov.path]: cov })
+      }
+      return Promise.resolve()
+    })
+  )
 
   await browser.close()
   ls.close(process.exit)
